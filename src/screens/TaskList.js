f48925a898863e544fcc7e8b52500a3a08bc2625
Thiago Marques <thiagossmarques@gmail.com>
2023-09-02 07:40:33 -0300

    feat: insere projeto base e configura ambiente

---
+++ src/screens/TaskList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+
+export default class TaskList extends Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <View>
+                    <Text>TaskList</Text>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}

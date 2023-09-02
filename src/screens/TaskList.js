2b97b5fbf20fa5659b32a52edb7a439e4a79bf8d
Thiago Marques <thiagossmarques@gmail.com>
2023-09-02 08:05:59 -0300

    feat: adiciona header

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react'
 import { View, Text, SafeAreaView, ImageBackground, StyleSheet } from 'react-native'
+
 import todayImage from '../../assets/imgs/today.jpg'
+
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+import commonStyles from '../commonStyles'
 
 export default class TaskList extends Component {
     render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <SafeAreaView style={styles.container}>
                 <ImageBackground source={todayImage} style={styles.background}>
-
+                <View style={styles.titleBar}>
+                    <Text style={styles.title}>Hoje</Text>
+                    <Text style={styles.subtitle}>{today}</Text>
+                </View>
                 </ImageBackground>
                 <View style={styles.taskList}>
-                <View>
-                    <Text>TaskList</Text>
-                </View>
+                    <Text>Tarefa #01</Text>
+                    <Text>Tarefa #02</Text>
+                    <Text>Tarefa #03</Text>
+                    <Text>Tarefa #04</Text>
                 </View>
             </SafeAreaView>
         )
@@ -28,5 +39,23 @@ const styles = StyleSheet.create({
     },
     taskList: {
         flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 50,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 30
     }
 })

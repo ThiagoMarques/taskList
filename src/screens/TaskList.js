78ec9abae23523af19a498ee378915df533b89a8
Thiago Marques <thiagossmarques@gmail.com>
2023-09-02 07:53:46 -0300

    feat: insere tela de tasks

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ImageBackground, StyleSheet } from 'react-native'
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render() {
         return (
-            <SafeAreaView>
+            <SafeAreaView style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
                 <View>
                     <Text>TaskList</Text>
+                </View>
                 </View>
             </SafeAreaView>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    }
+})

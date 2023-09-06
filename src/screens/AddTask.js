262316a3b59f2288d67a8ae26b0739fa402d504c
Thiago Marques <thiagossmarques@gmail.com>
2023-09-06 07:56:36 -0300

    feat: adiciona estado para controlar input de texto

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,17 +1,46 @@
-import React, {Component} from 'react'
-import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import React, { Component } from 'react'
+import { Modal, View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput } from 'react-native'
+import commonStyles from '../commonStyles'
+
+const initialState = { desc: ''}
 
 export default class AddTask extends Component {
+
+    state = {
+        ...initialState
+    }
+
     render() {
         return (
-            <Modal transparent={true} 
-            visible={this.props.isVisible}
-            onRequestClose={this.props.onCancel}
-            animationType='slide'
+            <Modal transparent={true}
+                visible={this.props.isVisible}
+                onRequestClose={this.props.onCancel}
+                animationType='slide'
             >
-                <TouchableWithoutFeedback 
-                style={styles.background}
-                onPress={this.props.onCancel}>
+                <TouchableWithoutFeedback
+                    style={styles.background}
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View style={styles.container}>
+                    <Text style={styles.header}>Nova tarefa</Text>
+                    <TextInput style={styles.input} 
+                    placeholder='Descrição da tarefa'
+                    onChangeText={desc => this.setState({desc})}
+                    value={this.state.desc}
+                     />
+                    <View style={styles.buttons}>
+                    <TouchableOpacity>
+                            <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={this.props.onCancel}>
+                            <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <TouchableWithoutFeedback
+                    style={styles.background}
+                    onPress={this.props.onCancel}>
                     <View style={styles.background}></View>
                 </TouchableWithoutFeedback>
             </Modal>
@@ -23,5 +52,35 @@ const styles = StyleSheet.create({
     background: {
         flex: 1,
         backgroundColor: 'rgba(0,0,0,0.7)'
+    },
+    container: {
+        backgroundColor: '#FFF'
+    },
+    header: {
+        fontFamily: commonStyles.fontFamily,
+        backgroundColor: commonStyles.colors.today,
+        color: commonStyles.colors.secondary,
+        textAlign: 'center',
+        padding: 15,
+        fontSize: 18
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        width: '90%',
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    },
+    button: {
+        margin: 20,
+        marginRight: 30,
+        color: commonStyles.colors.today
     }
 })
